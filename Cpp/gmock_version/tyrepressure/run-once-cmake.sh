725aebae4fe20c59689b0e48a21f1b6e65cea717
Anders Arnholm <anders@arnholm.se>
2018-11-10 11:46:53 +0100

    Add simple coverage support to cpp-gmock tyre preasure.

--- Cpp/gmock_version/tyrepressure/run-once-cmake.sh
+++ Cpp/gmock_version/tyrepressure/run-once-cmake.sh
@@ -5,4 +5,4 @@ if [[ ! -d build ]]; then
 fi
 
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=DEBUG && cmake --build . && ctest --output-on-failure
+cmake .. -DCMAKE_BUILD_TYPE=DEBUG && cmake --build . && cmake --build . --target tyrepressure_html_coverage
